5ec93b79a51091e102c33d6db64819b6fec420fb
advamangoel <adv.amangoel@gmail.com>
2026-09-02 23:02:19 +0530

    Restore Android compile SDK configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,25 @@ plugins {
 }
 
 android {
-       compileOptions {
+    namespace = "com.ejagriti.casemanager"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ejagriti.casemanager"
+        minSdk = 24
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         jvmTarget = "17"
-    
     }
 
     buildFeatures {
@@ -33,13 +44,11 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-
     implementation("androidx.activity:activity-compose:1.9.3")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.navigation:navigation-compose:2.8.2")
